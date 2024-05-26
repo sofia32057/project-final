badb7c3c6d4565735980dae64df22d5bf0afb2ec
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-05-26 18:16:51 +0200

    Update - added get and post for guests + model

--- backend/README.md
+++ backend/README.md
@@ -6,3 +6,32 @@ This project includes the packages and babel setup for an express server, and is
 
 1.  Install the required dependencies using `npm install`.
 2.  Start the development server using `npm run dev`.
+
+## Endpoints and routes
+
+### /
+
+### /guests
+
+Example of guest
+
+```
+  {
+    "firstname": "Test2",
+    "lastname": "Testsson",
+    "email": "name@domain.io",
+    "password": "test",
+    "plusOne": {
+      "isAllowed": true,
+      "name": "Plus One",
+      "foodChoice": "Fish"
+    },
+    "speech": {
+      "isAllowed": true,
+      "willMakeSpeech": true
+    },
+    "foodChoice": "Vegetarian",
+    "relation": "Sister of the bride",
+    "willAttend": true
+  }
+```
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import bcrypt from "bcrypt";
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/our-wedding";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+mongoose.set("setDefaultsOnInsert", false);
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -15,7 +16,63 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+const Guest = model("Guest", {
+  accessToken: {
+    type: String,
+    default: () => bcrypt.genSaltSync(),
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  willAttend: {
+    type: Boolean,
+    default: false,
+  },
+  plusOne: {
+    isAllowed: { type: Boolean, default: false },
+    name: { type: String },
+    foodChoice: { type: String },
+  },
+  speech: {
+    isAllowed: {
+      type: Boolean,
+    },
+    willMakeSpeech: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  foodChoice: {
+    type: String,
+  },
+  relation: {
+    type: String,
+    required: true,
+  },
+  // Combine name a unique username
+  // name: {
+  //   type: String,
+  //   default: () => {
+  //     `${this.firstname} ${this.lastname}`;
+  //   },
+  //   unique: true,
+  // },
+});
+
+// ROUTES
 app.route("/").get(async (req, res) => {
   const endpoints = expressListEndpoints(app);
   res.json({
@@ -23,6 +80,51 @@ app.route("/").get(async (req, res) => {
     endpoints: endpoints,
   });
 });
+
+app
+  .route("/guests")
+  // Get all guests (specific fields)
+  .get(async (req, res) => {
+    const guests = await Guest.find(
+      {},
+      "firstname lastname relation willAttend"
+    );
+    res.send(guests);
+  })
+  // Post new guest
+  .post(async (req, res) => {
+    try {
+      // Deconstruct request body
+      const {
+        firstname,
+        lastname,
+        email,
+        password,
+        plusOne,
+        speech,
+        foodChoice,
+        relation,
+        willAttend,
+      } = req.body;
+
+      // Create and save new guest
+      const newGuest = await new Guest({
+        firstname,
+        lastname,
+        email,
+        password: bcrypt.hashSync(password, 10),
+        plusOne,
+        speech,
+        foodChoice,
+        relation,
+        willAttend,
+      }).save();
+
+      res.status(201).send(`Created guest ID ${newGuest._id}`);
+    } catch (err) {
+      res.status(400).json({ message: "Post request failed", error: err });
+    }
+  });
 
 // Start the server
 app.listen(port, () => {
