094260d00a10d135579c7b2fbda4b7bb6dd91f80
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-04 22:00:58 +0200

    added venue and map

--- frontend/src/sections/VenueSection.jsx
+++ frontend/src/sections/VenueSection.jsx
@@ -1,3 +1,15 @@
+import { Map } from "../components/Map";
+
 export const VenueSection = () => {
-  return <div>VenueSection</div>;
+  return (
+    <>
+      <h2>The Wedding Venue</h2>
+      <Map />
+      <div className="flex flex-col">
+        <p>
+          Venue address <br /> Wedding Street 888, 12345 Marriage City
+        </p>
+      </div>
+    </>
+  );
 };
