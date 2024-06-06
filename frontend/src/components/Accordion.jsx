4f617a80ca899bb36bed0abdd46392df198ddae0
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-06-06 20:03:22 +0200

    Update - changed text colors in accordion

--- frontend/src/components/Accordion.jsx
+++ frontend/src/components/Accordion.jsx
@@ -9,17 +9,17 @@ import { faq } from "../data/faq.json";
 export const Accordion = () => {
   return (
     <div className="w-full px-4 pt-32">
-      <div className="divide-white/5 bg-white/5 mx-auto w-full max-w-lg divide-y rounded-xl">
+      <div className="mx-auto w-full max-w-lg divide-y divide-white/5 rounded-xl bg-white/5">
         {faq.map((q) => {
           return (
-            <Disclosure as="div" className="p-6" defaultOpen={true}>
+            <Disclosure as="div" className="p-6" defaultOpen={false} key={q}>
               <DisclosureButton className="group flex w-full items-center justify-between">
-                <span className="text-white group-data-[hover]:text-white/80 text-sm/6 font-medium">
+                <span className="text-sm/6 font-medium text-gray-900 group-data-[hover]:text-white/80">
                   {q.question}
                 </span>
-                <ChevronDownIcon className="fill-white/60 group-data-[hover]:fill-white/50 size-5 group-data-[open]:rotate-180" />
+                <ChevronDownIcon className="group-data-[hover]:fill-gray/50 fill-gray/60 size-5 group-data-[open]:rotate-180" />
               </DisclosureButton>
-              <DisclosurePanel className="text-white/50 mt-2 text-sm/5">
+              <DisclosurePanel className="text-gray/50 mt-2 text-sm/5">
                 {q.answer}
               </DisclosurePanel>
             </Disclosure>
--- frontend/src/sections/Faq.jsx
+++ frontend/src/sections/Faq.jsx
@@ -3,9 +3,11 @@ import { Accordion } from "../components/Accordion";
 
 export const Faq = () => {
   return (
-    <section className="bg-white py-24 sm:py-32">
-      <Heading content={"FAQ"} level={2} />
-      <Accordion />
+    <section className="relative isolate flex flex-col overflow-hidden px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6 lg:py-24">
+        <Heading content={"FAQ"} level={2} />
+        <Accordion />
+      </div>
     </section>
   );
 };
