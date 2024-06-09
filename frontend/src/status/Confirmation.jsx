62c1619d33a679c960462356a580d2d56ce79660
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-09 22:24:59 +0200

    changed background color

--- frontend/src/status/Confirmation.jsx
+++ frontend/src/status/Confirmation.jsx
@@ -5,14 +5,14 @@ import { Button } from "../components/Button";
 export const Confirmation = () => {
   // const isAttending = useStore((state) => state.guestData.isAttending);
   return (
-    <section className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <section className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <Heading
           content={"Thanks for letting us know!"}
           level={1}
           pre={"Confirmation"}
         />
-        <p className="text-base mt-6 leading-7 text-gray-600">
+        <p className="mt-6 text-base leading-7 text-gray-600">
           This text is supposed to change depending on your answer. We haven't
           fixed that yet 🤷‍♀️
         </p>
