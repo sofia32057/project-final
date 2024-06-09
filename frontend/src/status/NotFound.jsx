556a995c26ab643215a8a4af4eea10da8d7618b3
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-09 20:03:52 +0200

    changing headings

--- frontend/src/status/NotFound.jsx
+++ frontend/src/status/NotFound.jsx
@@ -1,14 +1,12 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
 
 export const NotFound = () => {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
-        </h1>
+        <Heading content={"Page not found"} level={1} pre={"404"} />
         <p className="mt-6 text-base leading-7 text-gray-600">
           Sorry, we couldn’t find the page you’re looking for.
         </p>
@@ -20,8 +18,8 @@ export const NotFound = () => {
           >
             Go back home
           </a> */}
-          <a href="#" className="text-sm font-semibold text-gray-900">
-            Contact support <span aria-hidden="true">&rarr;</span>
+          <a href="/" className="text-sm font-semibold text-gray-900">
+            Back to homepage <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
       </div>
