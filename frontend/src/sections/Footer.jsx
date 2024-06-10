7719642acec3dade642653a69de24bf01c19ecaf
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-10 07:29:50 +0200

    adding a simple footer

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -1,3 +1,11 @@
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="p-2 text-right">
+      <p className="font-cormorant">
+        2024 © created by{" "}
+        <a href="https://sofias-portfolio.netlify.app/">Sofia</a> &{" "}
+        <a href="https://portfolio-nathalie.netlify.app/">Nathalie</a>
+      </p>
+    </div>
+  );
 };
