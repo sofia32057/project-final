998a4d02ee24621444d67aa9a846b15704ebc1ca
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-10 22:30:12 +0200

    grid styling and change of h3 heading

--- frontend/src/sections/VenueSection.jsx
+++ frontend/src/sections/VenueSection.jsx
@@ -4,7 +4,7 @@ import { MapPinIcon, RocketLaunchIcon } from "@heroicons/react/24/outline";
 
 export const VenueSection = () => {
   return (
-    <section className="grid px-6 py-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:grid-rows-2 lg:items-start lg:gap-y-10">
+    <section className="grid px-6 py-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start">
       <div className="lg:col-start-1 lg:row-end-2 lg:px-8">
         <Heading
           content={"The Wedding Venue"}
@@ -12,22 +12,21 @@ export const VenueSection = () => {
           pre={"Where forever starts"}
         />
       </div>
-      <div className="flex flex-col pt-6 md:pt-10 lg:row-span-2 ">
+      <div className="flex flex-col pt-6 md:pt-10 lg:sticky lg:row-span-2 ">
         <Map />
+        <p className="inline-flex gap-2 py-4 md:items-center md:justify-center lg:top-8 lg:px-8">
+          <MapPinIcon className=" h-8 w-8" /> Venue address: Castelnuovo
+          Tancredi, 53022 Buonconvento SI, Italy
+        </p>
       </div>
-      <p className="inline-flex gap-2 py-4 md:items-center md:justify-center lg:top-8 lg:col-start-1 lg:row-end-2 lg:self-end lg:justify-self-start lg:px-8">
-        <MapPinIcon className=" h-8 w-8" /> Venue address: Castelnuovo Tancredi,
-        53022 Buonconvento SI, Italy
-      </p>
-      <div className="lg:col-start-1 lg:row-end-3 lg:self-start lg:px-8">
+      <div className=" lg:px-8">
         <Heading content={"How to get there"} level={3} />
         <p className="mt-8 inline-flex flex-col">
-          <RocketLaunchIcon className="h-8 w-8" /> To reach Castelnuovo Tancredi
-          in Buonconvento, Italy, you can start from any major city in Italy. By
-          car, head in the direction of Tuscany, following signs that lead you
-          toward Siena. Once you are near Siena, continue driving towards
-          Buonconvento. As you get closer, look for signs or ask locals for
-          directions to Castelnuovo Tancredi.
+          To reach Castelnuovo Tancredi in Buonconvento, Italy, you can start
+          from any major city in Italy. By car, head in the direction of
+          Tuscany, following signs that lead you toward Siena. Once you are near
+          Siena, continue driving towards Buonconvento. As you get closer, look
+          for signs or ask locals for directions to Castelnuovo Tancredi.
           <p className="mt-8">
             If you prefer to bike, begin your journey in Tuscany. Follow the
             scenic routes that lead towards the countryside, making your way to
