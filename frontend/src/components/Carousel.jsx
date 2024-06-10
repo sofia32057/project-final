b0d1365e614396ec0c021be0ed31cebc7b4f2808
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-10 09:18:39 +0200

    trying to make slides work

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,4 +1,11 @@
 import { CarouselCard } from "./CarouselCard";
+import Slider from "react-slick";
+import Terrasse from "/anastasia-saldatava-DZp5uVfRneI-unsplash.jpg";
+import Garden from "/anastasia-saldatava-oNMk0OOul80-unsplash.jpg";
+import Landscape from "/doozydoom-_v5HCKSZOkA-unsplash.jpg";
+import DawnLandscape from "/giuseppe-mondi-fJWYwHWYQpY-unsplash.jpg";
+import HouseTerrasse from "/mattia-bericchia-fSfgZCofQz0-unsplash.jpg";
+import Pool from "/nicola-ricca-i2acnhnKssQ-unsplash.jpg";
 
 export const Carousel = () => {
   const settings = {
@@ -11,24 +18,12 @@ export const Carousel = () => {
 
   return (
     <Slider>
-      <div className="">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <img src="" alt="" />
-      </div>
+      <CarouselCard image={Terrasse} alt={"Terrasse with a view"} />
+      <CarouselCard image={Garden} alt={"Garden with table and chairs"} />
+      <CarouselCard image={Landscape} />
+      <CarouselCard image={DawnLandscape} />
+      <CarouselCard image={HouseTerrasse} />
+      <CarouselCard image={Pool} />
     </Slider>
   );
 };
--- frontend/src/components/CarouselCard.jsx
+++ frontend/src/components/CarouselCard.jsx
@@ -1,10 +1,9 @@
-import Terrasse from "/anastasia-saldatava-DZp5uVfRneI-unsplash.jpg";
-import Garden from "/anastasia-saldatava-oNMk0OOul80-unsplash.jpg";
-import Landscape from "/doozydoom-_v5HCKSZOkA-unsplash.jpg";
-import DawnLandscape from "/giuseppe-mondi-fJWYwHWYQpY-unsplash.jpg";
-import HouseTerrasse from "/mattia-bericchia-fSfgZCofQz0-unsplash.jpg";
-import Pool from "/nicola-ricca-i2acnhnKssQ-unsplash.jpg";
-
-export const CarouselCard = () => {
-  return <div>CarouselCard</div>;
+export const CarouselCard = ({ image, alt }) => {
+  return (
+    <div>
+      <div className="">
+        <img src={image} alt={alt} />
+      </div>
+    </div>
+  );
 };
