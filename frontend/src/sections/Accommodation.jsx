dbdb0a744dc6932c16d7bfa8bad8fef9e9858ea3
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-11 21:19:21 +0200

    change of map size

--- frontend/src/sections/Accommodation.jsx
+++ frontend/src/sections/Accommodation.jsx
@@ -4,7 +4,9 @@ import { Carousel } from "../components/Carousel";
 export const Accommodation = () => {
   return (
     <section className=" px-6 py-8 lg:px-8">
-      <Heading content={"Where to stay"} level={2} pre={"La dolce vita"} />
+      <div className="lg:ml-6">
+        <Heading content={"Where to stay"} level={2} pre={"La dolce vita"} />
+      </div>
       <Carousel />
     </section>
   );
