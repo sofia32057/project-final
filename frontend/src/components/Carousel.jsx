d86c82708236b9dc2b787cf7468a0e3a8b081899
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-11 18:43:33 +0200

    changed images and fixed slider size

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,13 +1,13 @@
-// import { CarouselCard } from "./CarouselCard";
+import { CarouselCard } from "./CarouselCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Terrasse from "/anastasia-saldatava-DZp5uVfRneI-unsplash.jpg";
 import Garden from "/anastasia-saldatava-oNMk0OOul80-unsplash.jpg";
 import Landscape from "/doozydoom-_v5HCKSZOkA-unsplash.jpg";
-import DawnLandscape from "/giuseppe-mondi-fJWYwHWYQpY-unsplash.jpg";
+import DinnerView from "/flo-p-pUor_oQHEG4-unsplash.jpg";
 import HouseTerrasse from "/mattia-bericchia-fSfgZCofQz0-unsplash.jpg";
-import Pool from "/nicola-ricca-i2acnhnKssQ-unsplash.jpg";
+import Pool from "/nick-george-cCb5SMpxusA-unsplash.jpg";
 
 export const Carousel = () => {
   const settings = {
@@ -16,55 +16,39 @@ export const Carousel = () => {
     speed: 500,
     centerPadding: "20px",
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
   };
 
   return (
     <Slider {...settings}>
-      <img
-        className="h-60 w-auto object-cover p-2"
-        src={Terrasse}
-        alt={"Terrasse with a view"}
-      />
-
-      <img
-        className="h-60 w-auto object-cover p-2"
-        src={Landscape}
-        alt={"Landscape of Tuscany"}
-      />
-
-      <img
-        className="h-60 w-auto object-cover p-2"
-        src={DawnLandscape}
-        alt={"View to Tuscan house at dawn"}
-      />
-
-      <img
-        className="h-60 w-auto object-cover p-2"
+      <CarouselCard src={Terrasse} alt={"Terrasse with a view"} />
+      <CarouselCard src={Landscape} alt={"Landscape of Tuscany"} />
+      <CarouselCard src={DinnerView} alt={"Dinner table with a view"} />
+      <CarouselCard
         src={HouseTerrasse}
         alt={"House with a terrasse in front"}
       />
-
-      <img
-        className="h-60 w-auto object-cover p-2"
-        src={Pool}
-        alt={"Poolside in Tuscany"}
-      />
-
-      <img
-        className="h-60 w-auto object-cover p-2"
-        src={Garden}
-        alt={"Garden with table and chairs"}
-      />
+      <CarouselCard src={Pool} alt={"Poolside in Tuscany"} />
+      <CarouselCard src={Garden} alt={"Garden with table and chairs"} />
     </Slider>
   );
 };
-
-// <Slider className="flex">
-//   <img src={Terrasse} alt={"Terrasse with a view"} />
-//   <img src={Garden} alt={"Garden with table and chairs"} />
-//   <img src={Landscape} alt={"Landscape of Tuscany"} />
-//   <img src={DawnLandscape} alt={"View to Tuscan house at dawn"} />
-//   <img src={HouseTerrasse} alt={"House with a terrasse in front"} />
-//   <img src={Pool} alt={"Poolside in Tuscany"} />
-// </Slider>
--- frontend/src/components/CarouselCard.jsx
+++ frontend/src/components/CarouselCard.jsx
@@ -1,10 +1,6 @@
-import Terrasse from "/anastasia-saldatava-DZp5uVfRneI-unsplash.jpg";
-import Garden from "/anastasia-saldatava-oNMk0OOul80-unsplash.jpg";
-import Landscape from "/doozydoom-_v5HCKSZOkA-unsplash.jpg";
-import DawnLandscape from "/giuseppe-mondi-fJWYwHWYQpY-unsplash.jpg";
-import HouseTerrasse from "/mattia-bericchia-fSfgZCofQz0-unsplash.jpg";
-import Pool from "/nicola-ricca-i2acnhnKssQ-unsplash.jpg";
+export const CarouselCard = ({ styling, src, alt }) => {
+  const defaultStyling = "h-80 w-auto rounded-xl object-cover p-1 md:h-90";
+  const style = styling || defaultStyling;
 
-export const CarouselCard = () => {
-  return <div></div>;
+  return <img className={style} src={src} alt={alt} />;
 };
