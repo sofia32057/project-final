d70566552cdbcf8fbbb2f56219c94a38eb1fe5ff
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-11 21:13:06 +0200

    accommodation responsiveness and deleting line of code in map

--- frontend/src/sections/Accommodation.jsx
+++ frontend/src/sections/Accommodation.jsx
@@ -3,7 +3,7 @@ import { Carousel } from "../components/Carousel";
 
 export const Accommodation = () => {
   return (
-    <section className="mx-auto max-w-screen-xl px-6 py-8 lg:px-8">
+    <section className=" px-6 py-8 lg:px-8">
       <Heading content={"Where to stay"} level={2} pre={"La dolce vita"} />
       <Carousel />
     </section>
