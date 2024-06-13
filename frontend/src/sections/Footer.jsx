9bf30f13e3c13528962c6e84b59f0412e22fbd52
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-06-13 22:48:22 +0200

    Update - rsvp button in footer

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -1,6 +1,9 @@
+import { Button } from "../components/Button";
+
 export const Footer = () => {
   return (
-    <div className="py-5 pr-6 text-right text-sm">
+    <div className="flex flex-col items-center justify-between gap-6 px-6 py-5 text-right text-sm md:flex-row ">
+      <Button label={"RSVP"} type={"link"} style={"primary"} action={"/rsvp"} />
       <p className="font-cormorant">
         ©2024 created by{" "}
         <a href="https://sofias-portfolio.netlify.app/">Sofia</a> &{" "}
