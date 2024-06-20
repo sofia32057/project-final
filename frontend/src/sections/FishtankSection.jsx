1b085da92bf9a6c7a33963e2360920bffa7715d4
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-06-20 14:59:59 +0200

    styling of quote

--- frontend/src/sections/FishtankSection.jsx
+++ frontend/src/sections/FishtankSection.jsx
@@ -1,7 +1,6 @@
 import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax";
 import CoupleImage from "/pexels-sandeep-malith-97607821-9361349.webp";
 import CoupleImage2 from "/pexels-asadphoto-1024984.webp";
-import { Heading } from "../components/Heading";
 
 export const FishtankSection = () => {
   return (
@@ -13,13 +12,16 @@ export const FishtankSection = () => {
             {
               speed: -15,
               children: (
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <Heading content={"We are celebrating"} level={2} />
+                <div className="absolute inset-0 flex items-center justify-center ">
+                  <p className="mx-8 text-center font-cormorant text-xl font-light leading-8 text-white">
+                    “Once in a while, right in the middle of an ordinary life,
+                    love gives us a fairy tale.”
+                  </p>
                 </div>
               ),
             },
           ]}
-          className="aspect-[2/1]"
+          className="aspect-[2/1.5]"
         />
       </ParallaxProvider>
     </>
